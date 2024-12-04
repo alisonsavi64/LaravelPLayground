44e90c09c3f03409b35bf27ddcdd74617aee609f
alisonsavi64 <alisonsavi64@gmail.com>
2024-12-04 20:12:18 -0300

    Laracast basic blades with tailwind

---
+++ example/resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+<x-slot:heading>Contact Page</x-slot:heading>
+    Hello from contact page!!
+</x-layout>
---
+++ example/resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>Home Page</x-slot:heading>
+    Hello from home page!!
+</x-layout>
